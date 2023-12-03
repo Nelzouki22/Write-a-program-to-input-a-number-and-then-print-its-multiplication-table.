1362d08c8b3dd4b4cb0a78f0aba83ddc63ffaa69
NADIR ELZOUKI <105569840+Nelzouki22@users.noreply.github.com>
2023-12-03 18:38:10 -0500

    Add files via upload

---
+++ programtoinputanumberandthenprintitsmultiplicationtable.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<math.h>
+int main ()
+{
+    int num1;
+    cin>>num1;
+    cout<<num1*2;
+    cout<<num1*3;
+    cout<<num1*4;
+    cout<<num1*5;
+    cout<<num1*6;
+    cout<<num1*7;
+    cout<<num1*8;
+    cout<<num1*9;
+
+}
